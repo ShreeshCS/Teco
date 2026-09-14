14855ee271f134d1773c2015da8717f8b0203a1c
Shreesh Lahauirya <shreeshlahauriya15@gmail.com>
2026-09-15 03:18:32 +0530

    feat: end-end implemented user routes and controller for fetching users

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,11 @@
 import express from 'express'
 import cors from 'cors'
+import userRouter from './routes/user.route.js'
+
 const app = express()
 
 app.use(cors({ origin: 'http://localhost:5173' }))
+app.use(userRouter)
 
 app.get('/health', (_request, response) => {
   response.json('Hello from Server!')
